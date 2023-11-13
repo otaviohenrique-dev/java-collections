0ce673e65992c4649c17785159931d67b2502935
otaviohenrique-dev <otaviohenrique.dev@outlook.com>
2023-11-13 08:04:30 -0300

    Criado arquivo ArrayList, aguardando implementação.

---
+++ src/list/ArrayList.java
@@ -0,0 +1,22 @@
+package list;
+
+/**
+ * Array List é utilizado para orderanção aleatória
+ * Rápido para consultas, entretanto um pouco lento para exclusões
+ * @author Otávio Henrique Dias
+ * Primeiro referencie a Interface e logo após a implentação;
+ * */
+
+public class ArrayList {
+    public static void main (String args[]){
+
+    }
+    private static void exemploListaSimples (){
+
+    }
+
+    private static void exemploListaSimplesOrdemAscendente(){
+
+    }
+
+}
