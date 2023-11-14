290edcc674ac71a9d993814a253f3190e77cd30d
Lorenzo Henrique <educsalaangra@gmail.com>
2023-11-14 13:44:31 -0300

    Implementado o método compare da classe compareNotaAluno.

--- src/domain/compareNotaAluno.java
+++ src/domain/compareNotaAluno.java
@@ -1,4 +1,13 @@
 package domain;
 
-public class compareNotaAluno {
+import list.ArrayListAluno;
+
+import java.util.Comparator;
+
+public class compareNotaAluno implements Comparator<ArrayListAluno> {
+
+    @Override
+    public int compare(ArrayListAluno o1, ArrayListAluno o2) {
+        return Double.compare(o1.getNota(), o2.getNota());
+    }
 }
