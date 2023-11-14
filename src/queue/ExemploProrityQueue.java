9d043514ea8b6acd3a5e7434ced2a6aa2d4e6738
Lorenzo Henrique <educsalaangra@gmail.com>
2023-11-14 14:45:54 -0300

    Implemented method ExemploProrityQueue.

--- src/queue/ExemploProrityQueue.java
+++ src/queue/ExemploProrityQueue.java
@@ -1,4 +1,20 @@
 package queue;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * @author Lorenzo Henrique
+ * **/
 public class ExemploProrityQueue {
+    public static void main (String args[]){
+        //Ordenação natural
+        Queue<String> queue = new PriorityQueue<String>();
+        queue.add("short");
+        queue.add("very long indeed");
+        queue.add("medium");
+        while (queue.size() != 0){
+            System.out.println(queue.remove());
+        }
+    }
 }
