2fcf99a913b11831b503fb49a0af3d5e72063406
Lorenzo Henrique <educsalaangra@gmail.com>
2023-11-14 12:49:22 -0300

    Implementado o método exemploListaAluno.

--- src/list/ArrayListExemplo.java
+++ src/list/ArrayListExemplo.java
@@ -15,8 +15,21 @@ public class ArrayListExemplo {
     public static void main (String args[]){
         exemploListaSimples();
         exemploListaSimplesOrdemAscendente();
+        exemploListaAluno();
 
     }
+
+    private static void exemploListaAluno() {
+        List <ArrayListAluno> lista = new ArrayList<ArrayListAluno>();
+        ArrayListAluno a = new ArrayListAluno("Otávio Henrique", "Microsoft Office", 0);
+        ArrayListAluno b = new ArrayListAluno("Diego Oliveira", "Linux Unbutu", 0);
+        ArrayListAluno c = new ArrayListAluno("Caio Fábio", "Desenvolvimento Web", 0);
+        lista.add(a);
+        lista.add(b);
+        lista.add(c);
+        System.out.println(lista);
+    }
+
     private static void exemploListaSimples (){
         List<String> lista = new ArrayList<String>();
         lista.add("Otávio Dias");
